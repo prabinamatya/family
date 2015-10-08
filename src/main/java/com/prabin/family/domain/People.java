48d3481c85b2b2d817dc51e1f151ead2279d947c
Prabin Amatya <pnamatya@gmail.com>
2015-10-07 19:55:08 -0500

    Added Entity annotation

--- src/main/java/com/prabin/family/domain/People.java
+++ src/main/java/com/prabin/family/domain/People.java
@@ -1,14 +1,18 @@
 package com.prabin.family.domain;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import jersey.repackaged.com.google.common.base.Objects;
 
 import org.hibernate.validator.constraints.NotEmpty;
 
+@Entity
+@Table(name="people")
 public class People {
 
 	@Id
