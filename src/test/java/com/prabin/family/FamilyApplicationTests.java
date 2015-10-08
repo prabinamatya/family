8bce1cf5f365fcc96bcb61c79c9ba31c41caecdf
Prabin Amatya <pnamatya@gmail.com>
2015-10-07 19:00:00 -0500

    initial directory setup

---
+++ src/test/java/com/prabin/family/FamilyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.prabin.family;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = FamilyApplication.class)
+public class FamilyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
