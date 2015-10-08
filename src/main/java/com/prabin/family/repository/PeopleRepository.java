a164c94040d4571b3816c226dbb140168e878b69
Prabin Amatya <pnamatya@gmail.com>
2015-10-07 19:47:55 -0500

    personrepository to handle crud

---
+++ src/main/java/com/prabin/family/repository/PeopleRepository.java
@@ -0,0 +1,9 @@
+package com.prabin.family.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.prabin.family.domain.People;
+
+public interface PeopleRepository extends CrudRepository<People, Integer>{
+
+}
