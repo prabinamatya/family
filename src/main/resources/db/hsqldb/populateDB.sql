b259c26394bf275c82733c45abbe8973cadbe41e
Prabin Amatya <pnamatya@gmail.com>
2015-10-08 10:13:38 -0500

    added address in db for populating it

--- src/main/resources/db/hsqldb/populateDB.sql
+++ src/main/resources/db/hsqldb/populateDB.sql
@@ -1,5 +1,5 @@
-INSERT INTO people VALUES (1, 'Prabin', 'Amatya');
-INSERT INTO people VALUES (2, 'James', 'May');
-INSERT INTO people VALUES (3, 'Richard', 'Hammond');
-INSERT INTO people VALUES (4, 'Jeremy', 'Clarkson');
-INSERT INTO people VALUES (5, 'John', 'Snow');
+INSERT INTO people VALUES (1, 'Prabin', 'Amatya', 'Nepal address');
+INSERT INTO people VALUES (2, 'James', 'May', 'UK address');
+INSERT INTO people VALUES (3, 'Richard', 'Hammond', 'Scotland address');
+INSERT INTO people VALUES (4, 'Jeremy', 'Clarkson', 'Ireland address');
+INSERT INTO people VALUES (5, 'John', 'Snow', 'know nothing address');
